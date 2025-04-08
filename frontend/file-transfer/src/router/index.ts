064236fe964262cbd541fba44fd1aec382321d18
EastonTTT <1074908183@qq.com>
2025-04-08 22:49:05 +0800

    frontend config init

---
+++ frontend/file-transfer/src/router/index.ts
@@ -0,0 +1,17 @@
+import { createWebHistory, createRouter, } from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('@/pages/tempHome.vue')
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
+
